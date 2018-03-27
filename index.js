a293c285466000210563a92b7f04202518fce2b3
Christoph Päper <bugzilla@crissov.de>
2018-03-27 21:06:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-module.exports = function markdownItUnderline (md) {
+module.exports = function markdownItEmph (md) {
 
   function renderEm (tokens, idx, opts, _, slf) {
     var token = tokens[idx];
     if (token.markup === '_') {
+      token.tag = 'i';
+    }
+    if (token.markup === '__') {
+      token.tag = 'b';
+    }
+    if (token.markup === '____') {
       token.tag = 'u';
     }
     return slf.renderToken(tokens, idx, opts);
